77664023adcef9bcaa55c3471ae31836cb25aebe
desirel <two_somang@icloud.com>
2021-05-10 23:23:04 +0900

    feat: 실전 프로젝트 생성

--- src/main/java/com/icloud/corespringsecurity/security/configs/SecurityConfig.java
+++ src/main/java/com/icloud/corespringsecurity/security/configs/SecurityConfig.java
@@ -1,2 +1,41 @@
-package com.icloud.corespringsecurity.security.configs;public class SecurityConfig {
+package com.icloud.corespringsecurity.security.configs;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+@RequiredArgsConstructor
+@Slf4j
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder encoder;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/mypage").hasRole("USER")
+                .antMatchers("/messages").hasRole("MANAGER")
+                .antMatchers("/config").hasRole("ADMIN")
+                .anyRequest().authenticated();
+
+
+        http.formLogin();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        String password = encoder.encode("1111");
+
+        auth.inMemoryAuthentication().withUser("user").password(password).roles("USER")
+                .and()
+                .withUser("manager").password(password).roles("USER", "MANAGER")
+                .and()
+                .withUser("admin").password(password).roles("USER", "MANAGER", "ADMIN");
+    }
 }
