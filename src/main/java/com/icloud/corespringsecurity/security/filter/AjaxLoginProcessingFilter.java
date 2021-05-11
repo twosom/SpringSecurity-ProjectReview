03179b7889d1d5f989bb09637e30178314bb3d5f
desirel <two_somang@icloud.com>
2021-05-12 05:29:04 +0900

    feat: AjaxAuthenticationFilter

---
+++ src/main/java/com/icloud/corespringsecurity/security/filter/AjaxLoginProcessingFilter.java
@@ -0,0 +1,55 @@
+package com.icloud.corespringsecurity.security.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.icloud.corespringsecurity.domain.AccountDto;
+import com.icloud.corespringsecurity.security.token.AjaxAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class AjaxLoginProcessingFilter extends AbstractAuthenticationProcessingFilter {
+
+    private final String XML_HTTP_REQUEST = "XMLHttpRequest";
+    private final String X_REQUESTED_WITH = "X-Requested-With";
+
+
+    public AjaxLoginProcessingFilter() {
+        super(new AntPathRequestMatcher("/api/login"));
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request,
+                                                HttpServletResponse response)
+                                                throws AuthenticationException, IOException, ServletException {
+
+        if (!isAjax(request)) {
+            throw new IllegalStateException("Authentication is not supported");
+        }
+
+        AccountDto accountDto = new ObjectMapper().readValue(request.getInputStream(), AccountDto.class);
+
+        if (!StringUtils.hasText(accountDto.getUsername()) || !StringUtils.hasText(accountDto.getPassword())) {
+            throw new IllegalArgumentException("Username or Password must not by empty");
+        }
+
+        return getAuthenticationManager()
+                .authenticate(
+                        new AjaxAuthenticationToken(
+                                accountDto.getUsername(),
+                                accountDto.getPassword()
+                        )
+                );
+    }
+
+    private boolean isAjax(HttpServletRequest request) {
+        return XML_HTTP_REQUEST.equals(request.getHeader(X_REQUESTED_WITH));
+    }
+}
