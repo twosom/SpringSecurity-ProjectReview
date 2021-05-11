50510405ef1db81590ec2b2f3d28a46fb5ea7457
desirel <two_somang@icloud.com>
2021-05-11 21:23:12 +0900

    feat: 인증 부가 기능

--- src/main/java/com/icloud/corespringsecurity/security/provider/CustomAuthenticationProvider.java
+++ src/main/java/com/icloud/corespringsecurity/security/provider/CustomAuthenticationProvider.java
@@ -1,15 +1,19 @@
 package com.icloud.corespringsecurity.security.provider;
 
+import com.icloud.corespringsecurity.security.common.FormAuthenticationDetailsSource;
+import com.icloud.corespringsecurity.security.common.FormWebAuthenticationDetails;
 import com.icloud.corespringsecurity.security.service.AccountContext;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.InsufficientAuthenticationException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.util.StringUtils;
 
 @RequiredArgsConstructor
 @Configuration
@@ -28,6 +32,13 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
         if (!encoder.matches(password, accountContext.getAccount().getPassword())) {
             throw new BadCredentialsException("BadCredentialsException");
         }
+
+        FormWebAuthenticationDetails details = (FormWebAuthenticationDetails) authentication.getDetails();
+        String secretKey = details.getSecretKey();
+        if (!StringUtils.hasText(secretKey) || !secretKey.equals("secret")) {
+            throw new InsufficientAuthenticationException("InsufficientAuthenticationException");
+        }
+
 
         return new UsernamePasswordAuthenticationToken(accountContext.getAccount(), null, accountContext.getAuthorities());
     }
